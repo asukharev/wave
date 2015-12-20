31dc2e755c53055712d414d646f8ee054e086fd7
Alexander Sukharev <asuk1979@gmail.com>
2015-12-20 19:39:18 +0300

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -3,85 +3,23 @@
 
 #![allow(dead_code)]
 
-use std::fs::File;
-use std::io::Write;
+mod wav;
 mod bits;
-use bits::{Bits};
-mod wave;
 mod vec_ext;
-use vec_ext::{VecExt};
-
-// 0x0001 	WAVE_FORMAT_PCM 	PCM
-// 0x0003 	WAVE_FORMAT_IEEE_FLOAT 	IEEE float
-// 0x0006 	WAVE_FORMAT_ALAW 	8-bit ITU-T G.711 A-law
-// 0x0007 	WAVE_FORMAT_MULAW 	8-bit ITU-T G.711 µ-law
-// 0xFFFE 	WAVE_FORMAT_EXTENSIBLE 	Determined by SubFormat
+mod wave;
 
 #[test]
 fn generate_wave() {
-    let sample_rate: u32 = 44100;
-    let channels: u16 = 1;
-    let bits_per_sample: u16 = 16;
-    let duration: f32 = 1.0 as f32;
+    use wave::Wave;
 
-    let wave = wave::sin(sample_rate, 2400, 1.0f32, duration);
+    let sample_rate = 8000;
+    let duration = 1.0 as f32;
+    let wave = Wave::sin("channel1", sample_rate, 2400, 1.0f32, duration);
+    // println!("{:?}", wave);
 
-    let mut header: Vec<u8> = vec![];
-    {
-        // sGroupID
-        header.push_all_ext("RIFF".as_bytes());
-        // dwFileLength
-        header.push_u32_le((44 + (wave.len() * 4)) as u32);
-        // sRiffType
-        header.push_all_ext("WAVE".as_bytes());
-    }
-    let mut fmt: Vec<u8> = vec![];
-    {
-        // sGroupID
-        fmt.push_all_ext("fmt ".as_bytes());
-        // dwChunkSize
-        fmt.push_u32_le(16 as u32);
-        // wFormatTag
-        fmt.push_u16_le(1); // 0x0001 WAVE_FORMAT_PCM PCM
-        // wChannels
-        fmt.push_u16_le(1);
-        // dwSamplesPerSec
-        fmt.push_u32_le(sample_rate);
-        // dwAvgBytesPerSec
-        fmt.push_u32_le(sample_rate as u32 * channels as u32 * bits_per_sample as u32 / 8);
-        // wBlockAlign
-        fmt.push_u16_le(channels * bits_per_sample / 8);
-        // dwBitsPerSample
-        fmt.push_u16_le(bits_per_sample);
-    }
-    let mut data: Vec<u8> = vec![];
-    {
-        // sGroupID
-        data.push_all_ext("data".as_bytes());
-        // dwChunkSize
-        let s = wave.len() as u32 * channels as u32 * bits_per_sample as u32 / 8;
-        data.push_u32_le(s as u32);
-        // sampleData
-        for i in 0..wave.len() {
-            let sample: i16 = (wave[i] * 32768 as f32) as i16;
-            data.push_i16_le(sample);
-        }
-    }
-
-    let mut out: Vec<u8> = Vec::new();
-    out.push_all_ext(&header);
-    out.push_all_ext(&fmt);
-    out.push_all_ext(&data);
-
-    let mut f: std::fs::File = match File::create("wave.wav") {
-        Ok(ff) => ff,
-        Err(e) => panic!("{:?}", e),
-    };
-
-    match f.write_all(&out[..]) {
-        Ok(_) => (),
-        Err(e) => panic!("{:?}", e),
-    }
+    let wav = wav::Wav::from(wave);
+    // println!("{:?}", wav);
+    wav.save();
 
     assert!(false);
 }
--- src/wave.rs
+++ src/wave.rs
@@ -1,11 +1,43 @@
 use std::f32::consts::PI;
+use std::default::Default;
 
-pub struct Wave(pub Vec<u8>);
+#[derive(Debug)]
+pub struct Wave {
+    name: String,
+    pub sample_rate: u32,
+    frequency: u32,
+    amplitude: f32,
+    duration: f32,
+    pub data: Vec<f32>,
+}
 
-pub fn sin(sample_rate: u32, frequency: u32, amplitude: f32, duration: f32) -> Vec<f32> {
-    let samples_count = (sample_rate as f32 * duration) as u32;
-    (0..samples_count).map(|x| {
-        amplitude * (2.0f32 * PI * frequency as f32 * x as f32 / sample_rate as f32).sin()
-    })
-    .collect()
+impl Wave {
+    pub fn sin(name: &str, sample_rate: u32, frequency: u32, amplitude: f32, duration: f32) -> Wave {
+        let samples_count = (sample_rate as f32 * duration) as u32;
+        let samples: Vec<f32> = (0..samples_count).map(|x| {
+            amplitude * (2.0f32 * PI * frequency as f32 * x as f32 / sample_rate as f32).sin()
+        })
+        .collect();
+        Wave {
+            name: name.to_string(),
+            sample_rate: sample_rate,
+            frequency: frequency,
+            amplitude: amplitude,
+            duration: duration,
+            data: samples,
+        }
+    }
+}
+
+impl Default for Wave {
+    fn default() -> Self {
+        Wave {
+            name: "default".to_string(),
+            sample_rate: 0,
+            frequency: 0,
+            amplitude: 0.0f32,
+            duration: 0.0f32,
+            data: Vec::new(),
+        }
+    }
 }
